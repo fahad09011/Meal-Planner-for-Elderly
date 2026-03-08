c7ee1a0d3806bc864dcb5c456703fd1b71b08139
MuhamadFahad-coo311349 <fahadimran038@gmail.com>
2026-03-08 21:49:40 +0000

    move api call to vercel's serverless function to avoid exposing api key

---
+++ api/meals.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+    const rawApiKey = process.env.VITE_SPOONACULAR_KEY.trim();
+const API_KEY= typeof rawApiKey === "string" ? rawApiKey.trim() : "";
+  if (!API_KEY) {
+    return res
+      .status(500)
+      .json({ error: "spoonacular-API_KEY is not set on server" });
+  }
+  const { type = "breakfast,main course,side dish" } = req.query || {};
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeNutrition=true&addRecipeInformation=true&addRecipeInstructions=true&${encodeURIComponent(type)}&fillIngredients=true`;
+
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: data?.message });
+    }
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error("Serverless Spoonacular error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
