d5e90e33fee12b49a12ea855ec0dd6cfb2d7d0ce
MuhamadFahad-coo311349 <fahadimran038@gmail.com>
2026-03-25 13:48:32 +0000

    fix navbar collapse on small screen

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from "react";
 import icon from '../../assets/icons/userIcon.png';
 import logo from '../../assets/icons/logo.png';
 import '../../assets/styles/navBar.css';
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 function Navbar() {
   const { user, authLoading, signOut } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
+  const [navOpen, setNavOpen] = useState(false);
+
+  useEffect(() => {
+    setNavOpen(false);
+  }, [location.pathname]);
 
   const displayLabel =
     user?.user_metadata?.full_name?.trim() || user?.email || "";
@@ -21,10 +28,17 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0">
   <div className="container-fluid">
     <NavLink className="navbar-brand" to="/home"><img src={logo} alt="lopgo" className="logo" /> MealCare</NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <button
+      className="navbar-toggler"
+      type="button"
+      onClick={() => setNavOpen((open) => !open)}
+      aria-controls="navbarText"
+      aria-expanded={navOpen}
+      aria-label="Toggle navigation"
+    >
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarText">
+    <div className={`collapse navbar-collapse${navOpen ? " show" : ""}`} id="navbarText">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <NavLink className="nav-link" to="/home">Home</NavLink>
